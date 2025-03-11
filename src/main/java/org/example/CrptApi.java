0ba12a36365575a6f320fa6e315a6be261eb68ce
IvanovVV <Microd@inbox.ru>
2025-03-11 12:10:06 +0500

    Test 123

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -67,7 +67,7 @@ public class CrptApi {
         private final Document document;
         private final String sign;
     }
-//dasdas
+//dasdasdxasdaqs dsfdsfdsf dfssfs
     private HttpRequest buildRequest(String json) {
         return HttpRequest.newBuilder()
                 .uri(URI.create(URL))
